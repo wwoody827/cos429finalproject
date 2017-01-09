808f3c6bcb1ad653a9ac61dee879f05798c25343
Wudi Wang <wwoody827@gmail.com>
2017-01-08 23:02:53 -0500

    airplane 2

--- predict.py
+++ predict.py
@@ -62,7 +62,7 @@ predicts = net.inference(image)
 
 sess = tf.Session()
 
-np_img = cv2.imread('testimage/airplane.jpg')
+np_img = cv2.imread('testimage/airplane2.jpg')
 resized_img = cv2.resize(np_img, (448, 448))
 np_img = cv2.cvtColor(resized_img, cv2.COLOR_BGR2RGB)
 
